9bc8b4137ed0db35cd6fe60963b14b244f2c9155
mnematolahi <116376570+mnematolahi@users.noreply.github.com>
2025-09-23 17:53:57 +0330

    Create a-llm.py
    نسخه تغییر یافته برای متیس

---
+++ ch1/py-ir/a-llm.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai.chat_models import ChatOpenAI
+
+load_dotenv()
+
+metis_api_key = os.getenv("METIS_API_KEY")
+metis_base_url = "https://api.metisai.ir/openai/v1"
+
+model = ChatOpenAI(
+    model="gpt-4o",
+    openai_api_key=metis_api_key,
+    openai_api_base=metis_base_url,
+  # به علت محدودیت توکن عدد 5 قرار گرفته ولی شما می توانید اگر محدودیت هزینه ندارید خط زیر را حذف نمایید.
+    max_completion_tokens=5
+)
+
+print(" sending info to llm ...")
+
+response = model.invoke("The sky is")
+print(response.content)
