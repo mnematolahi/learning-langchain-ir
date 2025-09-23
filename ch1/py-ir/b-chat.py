0fbe13f08bbda6302b0bd90b24a07481acbb1654
mnematolahi <116376570+mnematolahi@users.noreply.github.com>
2025-09-23 18:51:33 +0330

    Update b-chat.py

--- ch1/py-ir/b-chat.py
+++ ch1/py-ir/b-chat.py
@@ -1,1 +1,19 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage
+load_dotenv()
 
+metis_api_key = os.getenv("METIS_API_KEY")
+metis_base_url = "https://api.metisai.ir/openai/v1"
+
+model = ChatOpenAI(
+    model="gpt-4o",
+    openai_api_key=metis_api_key,
+    openai_api_base=metis_base_url,
+    max_completion_tokens=10
+)
+
+prompt = [HumanMessage("what is the capital of france?")]
+response = model.invoke(prompt)
+print(response.content)
